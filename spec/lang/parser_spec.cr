7a7061b13e8e0b67f8cb96c12e7114f8c1517929
Kingsley <kingsleyhendrickse@me.com>
2022-01-31 16:20:09 +0000

    format

--- spec/lang/parser_spec.cr
+++ spec/lang/parser_spec.cr
@@ -25,13 +25,13 @@ describe Parser do
         ident = axal_ident.new("my_var")
         num = axal_num.new(1.0)
         var_binding = axal_var_binding.new(ident, num)
-        expected_programram = axal_prog.new
-        expected_programram.expressions << var_binding
+        expected_program = axal_prog.new
+        expected_program.expressions << var_binding
 
         parser = Parser.new(tokens_from_source("var_binding_ok_1.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "results in a syntax error when the syntax is not respected" do
@@ -48,315 +48,315 @@ describe Parser do
         ident = axal_ident.new("my_var")
         num = axal_num.new(1.0)
         var_binding = axal_var_binding.new(ident, num)
-        expected_programram = axal_prog.new
-        expected_programram.expressions << var_binding
-        expected_programram.expressions << ident
+        expected_program = axal_prog.new
+        expected_program.expressions << var_binding
+        expected_program.expressions << ident
 
         parser = Parser.new(tokens_from_source("standalone_identifier_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "standalone number" do
       it "generates the expected AST" do
-        expected_programram = axal_prog.new
-        expected_programram.expressions << axal_num.new(1991.0)
-        expected_programram.expressions << axal_num.new(7.0)
-        expected_programram.expressions << axal_num.new(28.28)
+        expected_program = axal_prog.new
+        expected_program.expressions << axal_num.new(1991.0)
+        expected_program.expressions << axal_num.new(7.0)
+        expected_program.expressions << axal_num.new(28.28)
 
         parser = Parser.new(tokens_from_source("standalone_number_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "standalone string" do
       it "generates the expected AST" do
-        expected_programram = axal_prog.new
-        expected_programram.expressions << axal_str.new("a string")
-        expected_programram.expressions << axal_str.new("another string")
+        expected_program = axal_prog.new
+        expected_program.expressions << axal_str.new("a string")
+        expected_program.expressions << axal_str.new("another string")
 
         parser = Parser.new(tokens_from_source("standalone_string_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "standalone boolean" do
       it "generates the expected AST" do
-        expected_programram = axal_prog.new
-        expected_programram.expressions << axal_bool.new(true)
-        expected_programram.expressions << axal_bool.new(false)
+        expected_program = axal_prog.new
+        expected_program.expressions << axal_bool.new(true)
+        expected_program.expressions << axal_bool.new(false)
 
         parser = Parser.new(tokens_from_source("standalone_boolean_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "boolean expressions" do
       it "generates the expected AST for 1 == 1" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         comparison_op = axal_binary_op.new(TokenKind::DOUBLE_EQUALS, axal_num.new(1.0), axal_num.new(1.0))
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_7.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for 2 != 1" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         comparison_op = axal_binary_op.new(TokenKind::NOT_EQUAL, axal_num.new(2.0), axal_num.new(1.0))
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_6.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for 2 + 2 > 3" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), axal_num.new(2.0))
         comparison_op = axal_binary_op.new(TokenKind::GREATER_THAN, plus_op, axal_num.new(3.0))
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_5.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for 2 + 2 >= 3" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), axal_num.new(2.0))
         comparison_op = axal_binary_op.new(TokenKind::GREATER_THAN_OR_EQUAL, plus_op, axal_num.new(3.0))
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_4.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for 3 < 2 + 2" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), axal_num.new(2.0))
         comparison_op = axal_binary_op.new(TokenKind::LESS_THAN, axal_num.new(3.0), plus_op)
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_3.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generate the expected AST for 3 <= 2 + 2" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), axal_num.new(2.0))
         comparison_op = axal_binary_op.new(TokenKind::LESS_THAN_OR_EQUAL, axal_num.new(3.0), plus_op)
 
-        expected_programram.expressions << comparison_op
+        expected_program.expressions << comparison_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_2.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for true != false" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         not_eq_op = axal_binary_op.new(TokenKind::NOT_EQUAL, axal_bool.new(true), axal_bool.new(false))
 
-        expected_programram.expressions << not_eq_op
+        expected_program.expressions << not_eq_op
         parser = Parser.new(tokens_from_source("boolean_expr_ok_1.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "standalone nil" do
       it "generates the expected AST" do
-        expected_programram = axal_prog.new
-        expected_programram.expressions << axal_nil.new
+        expected_program = axal_prog.new
+        expected_program.expressions << axal_nil.new
         parser = Parser.new(tokens_from_source("standalone_nil_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "unary operators" do
       it "generates the expected AST" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         minus_op_1 = axal_unary_op.new(TokenKind::HYPHEN, axal_num.new(28.0))
         minus_op_2 = axal_unary_op.new(TokenKind::HYPHEN, axal_num.new(24.42))
         bang_op = axal_unary_op.new(TokenKind::EXCLAMATION, axal_num.new(7.0))
-        expected_programram.expressions << minus_op_1
-        expected_programram.expressions << minus_op_2
-        expected_programram.expressions << bang_op
+        expected_program.expressions << minus_op_1
+        expected_program.expressions << minus_op_2
+        expected_program.expressions << bang_op
         parser = Parser.new(tokens_from_source("unary_operator_ok.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "binary operators" do
       it "generates the expected AST for 2 + 2" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), axal_num.new(2.0))
-        expected_programram.expressions << plus_op
+        expected_program.expressions << plus_op
         parser = Parser.new(tokens_from_source("binary_operator_ok_1.axal"))
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for 2 + 2 * 3" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         multiplication_op = axal_binary_op.new(TokenKind::ASTERISK, axal_num.new(2.0), axal_num.new(3.0))
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), multiplication_op)
-        expected_programram.expressions << plus_op
+        expected_program.expressions << plus_op
         parser = Parser.new(tokens_from_source("binary_operator_ok_2.axal"))
         parser.parse
 
         # expected: 2 + (2 * 3)
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "does generate the expected AST for 2 + 2 * 3 / 3" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         multiplication_op = axal_binary_op.new(TokenKind::ASTERISK, axal_num.new(2.0), axal_num.new(3.0))
         division_op = axal_binary_op.new(TokenKind::FORWARD_SLASH, multiplication_op, axal_num.new(3.0))
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(2.0), division_op)
-        expected_programram.expressions << plus_op
+        expected_program.expressions << plus_op
         parser = Parser.new(tokens_from_source("binary_operator_ok_3.axal"))
 
         parser.parse
 
         # expected: 2 + ((2 * 3) / 3)
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "mixed operators" do
       it "generates the expected AST for -10 + 2 + 2 * 3 / 3" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         inversion_op = axal_unary_op.new(TokenKind::HYPHEN, axal_num.new(10.0))
         plus_op_1 = axal_binary_op.new(TokenKind::PLUS, inversion_op, axal_num.new(2.0))
         multiplication_op = axal_binary_op.new(TokenKind::ASTERISK, axal_num.new(2.0), axal_num.new(3.0))
         division_op = axal_binary_op.new(TokenKind::FORWARD_SLASH, multiplication_op, axal_num.new(3.0))
         plus_op_2 = axal_binary_op.new(TokenKind::PLUS, plus_op_1, division_op)
-        expected_programram.expressions << plus_op_2
+        expected_program.expressions << plus_op_2
         parser = Parser.new(tokens_from_source("mixed_operator_ok_1.axal"))
 
         parser.parse
 
         # expected: (-10 + 2) + ((2 * 3) / 3)
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "logical operators" do
       it "generates the expected AST for true and false" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         and_op = axal_binary_op.new(TokenKind::AND, axal_bool.new(true), axal_bool.new(false))
-        expected_programram.expressions << and_op
+        expected_program.expressions << and_op
         parser = Parser.new(tokens_from_source("logical_operator_ok_1.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "does generate the expected AST for true or false" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         or_op = axal_binary_op.new(TokenKind::OR, axal_bool.new(true), axal_bool.new(false))
-        expected_programram.expressions << or_op
+        expected_program.expressions << or_op
         parser = Parser.new(tokens_from_source("logical_operator_ok_2.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "does generate the expected AST for true and false or true" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         and_op = axal_binary_op.new(TokenKind::AND, axal_bool.new(true), axal_bool.new(false))
         or_op = axal_binary_op.new(TokenKind::OR, and_op, axal_bool.new(true))
-        expected_programram.expressions << or_op
+        expected_program.expressions << or_op
         parser = Parser.new(tokens_from_source("logical_operator_ok_3.axal"))
 
         parser.parse
 
         # expected: (true and false) or true
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "grouped expressions" do
       it "generates the expected AST for (3 + 4) * 2" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(3.0), axal_num.new(4.0))
         multiplication_op = axal_binary_op.new(TokenKind::ASTERISK, plus_op, axal_num.new(2.0))
-        expected_programram.expressions << multiplication_op
+        expected_program.expressions << multiplication_op
         parser = Parser.new(tokens_from_source("grouped_expr_ok_1.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for ((4 + 4) / 4) * 2" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         plus_op = axal_binary_op.new(TokenKind::PLUS, axal_num.new(4.0), axal_num.new(4.0))
         division_op = axal_binary_op.new(TokenKind::FORWARD_SLASH, plus_op, axal_num.new(4.0))
         multiplication_op = axal_binary_op.new(TokenKind::ASTERISK, division_op, axal_num.new(2.0))
-        expected_programram.expressions << multiplication_op
+        expected_program.expressions << multiplication_op
         parser = Parser.new(tokens_from_source("grouped_expr_ok_2.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "generates the expected AST for true and (false or true)" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         or_op = axal_binary_op.new(TokenKind::OR, axal_bool.new(false), axal_bool.new(true))
         and_op = axal_binary_op.new(TokenKind::AND, axal_bool.new(true), or_op)
-        expected_programram.expressions << and_op
+        expected_program.expressions << and_op
         parser = Parser.new(tokens_from_source("grouped_expr_ok_3.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "return" do
       it "generates the expected AST" do
         ret = axal_return.new(axal_num.new(1.0))
-        expected_programram = axal_prog.new
-        expected_programram.expressions << ret
+        expected_program = axal_prog.new
+        expected_program.expressions << ret
         parser = Parser.new(tokens_from_source("return_ok.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
     context "conditionals" do
       it "generates the expected AST for a IF THEN conditional" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
         ident = axal_ident.new("world_still_makes_sense")
         var_binding_outer = axal_var_binding.new(ident, axal_bool.new(false))
         eq_op = axal_binary_op.new(TokenKind::DOUBLE_EQUALS, axal_num.new(1.0), axal_num.new(1.0))
@@ -364,17 +364,17 @@ describe Parser do
         true_block = axal_block.new
         true_block.expressions << var_binding_inner
         conditional = axal_conditional.new(eq_op, true_block)
-        expected_programram.expressions << var_binding_outer
-        expected_programram.expressions << conditional
+        expected_program.expressions << var_binding_outer
+        expected_program.expressions << conditional
         parser = Parser.new(tokens_from_source("conditional_ok_1.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "does generate the expected AST for a IF THEN ELSE conditional" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
 
         ident_1 = axal_ident.new("world_still_makes_sense")
         ident_2 = axal_ident.new("world_gone_mad")
@@ -394,18 +394,18 @@ describe Parser do
 
         conditional = axal_conditional.new(eq_op, true_block, false_block)
 
-        expected_programram.expressions << var_binding_1
-        expected_programram.expressions << var_binding_2
-        expected_programram.expressions << conditional
+        expected_program.expressions << var_binding_1
+        expected_program.expressions << var_binding_2
+        expected_program.expressions << conditional
         parser = Parser.new(tokens_from_source("conditional_ok_2.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
 
       it "does generate the expected AST for a IF THEN ELSE IF conditional" do
-        expected_programram = axal_prog.new
+        expected_program = axal_prog.new
 
         ident_1 = axal_ident.new("world_still_makes_sense")
         ident_2 = axal_ident.new("world_gone_mad")
@@ -426,14 +426,14 @@ describe Parser do
 
         conditional_outer = axal_conditional.new(eq_op, true_block, false_block)
 
-        expected_programram.expressions << var_binding_1
-        expected_programram.expressions << var_binding_2
-        expected_programram.expressions << conditional_outer
+        expected_program.expressions << var_binding_1
+        expected_program.expressions << var_binding_2
+        expected_program.expressions << conditional_outer
         parser = Parser.new(tokens_from_source("conditional_ok_3.axal"))
 
         parser.parse
 
-        parser.ast.should eq(expected_programram)
+        parser.ast.should eq(expected_program)
       end
     end
 
